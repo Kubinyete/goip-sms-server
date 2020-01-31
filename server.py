835eba870aedf09a6f5a89761b8da144775bfa0b
Kubinyete <vitorkubinyete@hotmail.com>
2020-01-31 18:28:40 -0200

    Estruturado loop de execucao do server

--- server.py
+++ server.py
@@ -2,7 +2,11 @@
 
 import socket
 import sys
+import select
 from datetime import datetime
+
+class SMSServerParseError(Exception):
+	pass
 
 class SMSAuthClient:
 	def __init__(self, username, password):
@@ -49,7 +53,7 @@ class SMSClient:
 
 	def send_package(self, currsocket, package):
 		packagestr = SMSServer.create_package(package)
-		currsocket.sendto(packagestr.encode('utf-8'), self.srcaddr)
+		currsocket.sendto(packagestr.encode('utf-8'), socket.MSG_DONTWAIT, self.srcaddr)
 
 class SMSServer:
 	def __init__(self, bindaddr, port, buffersize=2048):
@@ -67,11 +71,13 @@ class SMSServer:
 			"centralvox": SMSAuthClient('centralvox', 'centralvox')
 		}
 
-	def log(self, message, srcaddr=None, timestamp=datetime.now()):
+	def log(self, message, srcaddr=None):
+		timestamp = datetime.now()
+
 		print(f"[{timestamp}] {message}" if srcaddr is None else f"[{timestamp}] <{srcaddr[0]}:{srcaddr[1]}> {message}")
 
 	def listen(self):
-		print(f"Escutando em {self.bindaddr}, porta {self.port}...")
+		print(f"Escutando em '{self.bindaddr}', porta {self.port}...")
 
 		self.soc = socket.socket(
 			socket.AF_INET, 		# IPV4
@@ -84,51 +90,51 @@ class SMSServer:
 
 		try:
 			while True:
-				# BLoqueia a execução caso nao tenha nada a ser lido.
-				data, src = self.soc.recvfrom(self.buffersize)
+				if self.soc in select.select([self.soc], [], [], 0)[0]:
+					# Bloqueia a execução caso nao tenha nada a ser lido.
+					data, src = self.soc.recvfrom(self.buffersize)
 				
-				try:
-					data = data.decode('utf-8')
-				except Exception:
-					data = ""
-				
-				if data:
-					self.log(f"Recebido dados:\n{data}", src)
-
-					package = None
-					
 					try:
-						package = self.parse_package(data)
-					except ValueError as e:
-						self.log(f"Falha ao efetuar parse_package: {e}", src)
+						data = data.decode('utf-8')
+					
+						if data:
+							self.log(f"Recebido dados:\n{data}", src)
 
-					if package:
-						if 'req' in package:
-							auth_client = None
-							sms_client = None
+							package = self.parse_package(data)
 
-							# Registro/Keep-alive
-							if package['id'] in self.auth_clients:
-								auth_client = self.auth_clients[package['id']]
-								sms_client = auth_client.authenticate(package['id'], package['pass'], src)
-							
-							if auth_client:
-								if sms_client:
-									self.log(f"Registrado heartbeat do ID '{package['id']}'.", src)
-									
-									sms_client.send_package(self.soc, {
-										"reg": package['req'],
-										"status": 200
-									})
+							# Tentativa de registro/keep-alive
+							if 'req' in package:
+								auth_client = None
+								sms_client = None
+
+								# ID existe?
+								if package['id'] in self.auth_clients:
+									auth_client = self.auth_clients[package['id']]
+									sms_client = auth_client.authenticate(package['id'], package['pass'], src)
+								
+								if auth_client:
+									if sms_client:
+										self.log(f"Registrado heartbeat do ID '{package['id']}'.", src)
+										
+										sms_client.send_package(self.soc, {
+											"reg": package['req'],
+											"status": 200
+										})
+									else:
+										self.log(f"Falha na autenticação do ID '{package['id']}': Senha incorreta.", src)
 								else:
-									self.log(f"Falha na autenticação do ID '{package['id']}': Senha incorreta.", src)
-							else:
-								self.log(f"Falha na autenticação do ID '{package['id']}': Usuário inexistente.", src)
-							
+									self.log(f"Falha na autenticação do ID '{package['id']}': Usuário inexistente.", src)
+					except UnicodeDecodeError as e:
+						self.log(f"Nao foi possível decodificar os dados recebidos, unicode esperado: {e}", src)
+					except KeyError as e:
+						self.log(f"Esperado chave do cliente, porém inexistente: {e}", src)
+					except SMSServerParseError as e:
+						self.log(f"Falha ao efetuar parse_package: {e}", src)
+				else:
+					# Verificar se existem solicitações de SMS a serem enviadas
+					pass
 		except KeyboardInterrupt:
 			self.stop()
-		except KeyError as e:
-			self.log(f"Esperado chave do cliente, porém inexistente: {e}")
 
 	def stop(self):
 		self.soc.close()
@@ -145,7 +151,7 @@ class SMSServer:
 				if split:
 					ret[split[0]] = ":".join(split[1:]) if len(split) > 1 else ""
 		else:
-			raise ValueError("Cojunto de chaves e valores está incorreto e/ou incompleto.")
+			raise SMSServerParseError("Cojunto de chaves e valores está incorreto e/ou incompleto.")
 
 		return ret
 
